908b6d385e8f2e9c8a9b2df8383888e16ab5429e
roger <roger.z.xu@gmail.com>
2015-06-25 15:27:08 -0400

    simple python webserver with index.html

---
+++ server.py
@@ -0,0 +1,13 @@
+__author__ = 'Roger'
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
